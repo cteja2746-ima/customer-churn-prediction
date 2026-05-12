ce597ad471135a06e9f187d37ce5aba6e040e3c8
Charan Teja <cteja2746@gmail.com>
2026-05-12 17:15:11 +0530

    Added advanced EDA visualizations

--- load.py
+++ load.py
@@ -1,15 +1,12 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Load dataset
 df = pd.read_csv("Data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
-# PostgreSQL connection
 engine = create_engine(
     "postgresql://postgres:charan%4014@localhost:5433/CUSTOMER_CHURN_PROJECT"
 )
 
-# Upload dataset
 df.to_sql(
     "customer_churn",
     engine,
